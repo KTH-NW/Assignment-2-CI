346b57602e403a18ac109f60e065a54d38aaa45e
nwessman <nwessman@kth.se>
2021-02-11 00:40:15 +0100

    Add function to list files in a folder
    This function returns all files in a folder as a list of File.

--- app/src/main/java/ci/FileIO.java
+++ app/src/main/java/ci/FileIO.java
@@ -8,4 +8,17 @@ public class FileIO {
 
 
 
+    /**
+     * 
+     * @param folderPath The folder we want to look at
+     * @return An array of File with all files in the folder
+     */
+    public static File[] listFileNames(String folderPath){
+        File[] pathnames;
+        File f = new File(folderPath);
+        pathnames = f.listFiles();
+
+        return pathnames; 
+    }
+
 }
