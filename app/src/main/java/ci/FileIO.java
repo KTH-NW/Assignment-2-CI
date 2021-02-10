0af552078463c7324a136ab0b5b1cede69621ecd
nwessman <nwessman@kth.se>
2021-02-11 00:41:31 +0100

    Adds function for getting content of file
    This adds a function that returns the content of a file as a string.

--- app/src/main/java/ci/FileIO.java
+++ app/src/main/java/ci/FileIO.java
@@ -8,6 +8,33 @@ public class FileIO {
 
 
 
+     /**
+     * 
+     * @param pathname The file that we want to inspect
+     * @return The content of the file as a string
+     */
+    public static String contentOfFile(File pathname){
+        StringBuilder sb = new StringBuilder();
+
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(pathname));
+            String str;
+            while((str = br.readLine()) != null){
+                sb.append(str);
+            }
+            br.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println(e);
+            return "";
+        }
+        catch(IOException e){
+            System.out.println(e);
+            return "";
+        }
+        return sb.toString();
+    }
+
     /**
      * 
      * @param folderPath The folder we want to look at
